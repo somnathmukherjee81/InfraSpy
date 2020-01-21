4bdd6809ae22bb13e5ddbc09bd9f5f5a74da4ea0
Somnath Mukherjee <somnathmukherjee81@gmail.com>
2020-01-21 15:39:41 -0700

    feat(infraspy): initial code base

---
+++ InfraSpy.Core/Client.cs
@@ -0,0 +1,82 @@
+﻿namespace InfraSpy.Core
+{
+    using k8s;
+    using k8s.Models;
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Text;
+
+    public sealed class K8SClient
+    {
+        private KubernetesClientConfiguration config;
+        private IKubernetes client;
+
+        /// <summary>
+        /// Prevents a default instance of the <see cref="K8SClient"/> class from being created.
+        /// </summary>
+        private K8SClient()
+        {
+            config = KubernetesClientConfiguration.BuildConfigFromConfigFile();
+            client = new Kubernetes(config);
+        }
+
+        /// <summary>
+        /// Nested class for lazy instantiation of singleton
+        /// </summary>
+        private class NestedClient
+        {
+            /// <summary>
+            /// Initializes the <see cref="NestedClient"/> class.
+            /// </summary>
+            static NestedClient()
+            { }
+
+            internal static readonly K8SClient k8sClient = new K8SClient();
+        }
+
+        public static K8SClient Instance { get => NestedClient.k8sClient; }
+
+        public IList<V1Namespace> Namespaces { get => client.ListNamespace().Items; }
+
+        public IList<V1Pod> GetPods(string ns)
+        {
+            return client.ListNamespacedPod(ns).Items;
+        }
+
+        public IList<V1Deployment> GetDeployments(string ns)
+        {            
+            return client.ListNamespacedDeployment(ns).Items;
+        }
+
+        public string GetPodLogSnapshot(string podName, string ns, int? tailLines = null)
+        {
+            var stream = client.ReadNamespacedPodLog(podName, ns, tailLines: tailLines);
+
+            StreamReader reader = new StreamReader(stream);
+            string logText = reader.ReadToEnd();
+
+            return logText;
+        }
+
+        public string SearchInPodLog(string podName, string ns, string searchPhrase, int? tailLines = null)
+        {
+            var stream = client.ReadNamespacedPodLog(podName, ns, tailLines: tailLines);
+
+            StreamReader reader = new StreamReader(stream);
+            StringBuilder resultsBuilder = new StringBuilder();
+
+            string line;
+
+            while ((line = reader.ReadLine()) != null)
+            {
+                if(line.Contains(searchPhrase))
+                {
+                    resultsBuilder.AppendLine(line);
+                }
+            }
+
+            return resultsBuilder.ToString();
+        }
+    }
+}
---
+++ InfraSpy/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace InfraSpy
+{
+    using System;
+    using System.Globalization;
+    using InfraSpy.Core;
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string ns = "smartpoint-chandra";
+            Console.WriteLine($"Pods in {ns} namespace");
+            Console.WriteLine("==============================");
+            foreach (var pod in K8SClient.Instance.GetPods(ns))
+            {
+                Console.WriteLine(pod.Metadata.Name);
+            }
+
+            Console.Write("Enter pod name to get logs: ");
+            string podName = Console.ReadLine();
+
+            Console.Write("Enter number of lines to display: ");
+            string tail = Console.ReadLine();
+            int tailLines = Convert.ToInt32(tail, NumberFormatInfo.InvariantInfo);
+
+            string logText = K8SClient.Instance.GetPodLogSnapshot(podName, ns, tailLines);
+
+
+            Console.WriteLine($"{tailLines} lines of logs from {podName}");
+            Console.WriteLine("==============================");
+            Console.WriteLine(logText);
+
+            Console.Write("Search Phrase: ");
+            string searchPhrase = Console.ReadLine();
+            string searchResult = K8SClient.Instance.SearchInPodLog(podName, ns, searchPhrase, tailLines);
+
+            Console.WriteLine($"Searching the phrase '{searchPhrase}' in {tailLines} lines of logs from {podName}");
+            Console.WriteLine("==============================");
+            Console.WriteLine(searchResult);
+        }
+    }
+}
